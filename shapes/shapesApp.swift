aaa7c42a3c49d78dda2e12474b243decde21d3c1
Vinicio Brejinski <viniciosdb@gmail.com>
2023-02-24 15:07:04 -0300

    Initial Commit

---
+++ shapes/shapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  shapesApp.swift
+//  shapes
+//
+//  Created by Vinicio Brejinski on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct shapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
